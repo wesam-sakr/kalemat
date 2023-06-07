83b85a377c3ba8f2bb41b56cb2c76ae788731d71
wesam-sakr <wesamsakr801@gmail.com>
2023-06-07 16:12:36 +0300

    prof

--- js/script.js
+++ js/script.js
@@ -219,6 +219,31 @@ $(".show-pass").click(function () {
   $('.author-cover').height(authWidth)
   })
 
+  if ($(".profile").length > 0) {
+    const imgDiv = document.querySelector('.profile-pic');
+    const img = document.querySelector('#photo');
+    const file = document.querySelector('#file');
+    const uploadBtn = document.querySelector('#uploadBtn');
+    //if user hover on img div
+    imgDiv.addEventListener('mouseenter', function(){
+        uploadBtn.style.display = "block";
+    });
+    //if we hover out from img div
+    imgDiv.addEventListener('mouseleave', function(){
+        uploadBtn.style.display = "none";
+    });
+    //when we choose a pic to upload
+    file.addEventListener('change', function(){
+    const choosedFile = this.files[0];
+    if (choosedFile) {
+        const reader = new FileReader();
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+        reader.readAsDataURL(choosedFile);
+    }
+    });
+}
 
   // niceSelect
   $('select').niceSelect();
