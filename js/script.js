32d227e8111a104bc47b7ff6b24202a225ea3bcb
wesam-sakr <wesamsakr801@gmail.com>
2023-06-07 10:32:25 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,32 +1,55 @@
+// replace text
+$.fn.toggleText = function(t1, t2){
+  if(this.text() == t1){
+    this.text(t2);
+  }else{                   
+    this.text(t1);
+  }
+  return this;
+};
+
+// dir
+var bodyDir = $('body').css('direction')
+console.log(bodyDir)
+var dir
+if(bodyDir == "rtl"){
+  dir= true
+}
+else{
+  dir = false
+}
+
 $(document).ready(function () {
   // loading
-  $("#loading").fadeOut(1500);
+  setTimeout(function () {
+    $("#loading").fadeOut();
+  }, 3000);
 
+  // header vedio
   $(".play-icon").click(function(){
     document.querySelector(".header-video video").play();
     $(this).addClass("d-none");
     $(".pause-icon").removeClass("d-none");
     $('.video-cover-img').fadeOut(2000);
   })
- 
   $(".pause-icon").click(function(){
     document.querySelector(".header-video video").pause();
     $(this).addClass("d-none");
     $(".play-icon").removeClass("d-none");
     $('.video-cover-img').fadeIn(2000);
   })
-
   $('video').on('ended',function(){ 
     $('.video-cover-img').fadeIn(2000);
     $('.pause-icon').addClass("d-none");
     $(".play-icon").removeClass("d-none");
   });
 
+  // owl-carousel
   $('.partners-slider .owl-carousel').owlCarousel({
     loop:false,
     margin:20,
     responsiveClass:true,
-    rtl:true,
+    rtl: dir,
     autoplay:true,
     autoplayTimeout:20000,
     responsive:{
@@ -44,10 +67,29 @@ $(document).ready(function () {
         }
     }
   });
-
+  $('.grid-books .owl-carousel').owlCarousel({
+    loop:false,
+    nav: true,
+    margin:20,
+    responsiveClass:true,
+    rtl: dir,
+    autoplay:true,
+    autoplayTimeout:20000,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+  });
   $(".single_book_single .owl-carousel").owlCarousel({
     items: 2,
-    rtl: true,
+    rtl: dir,
     loop: true,
     margin: 15,
     nav: true,
@@ -67,10 +109,9 @@ $(document).ready(function () {
 
     },
   });
-  
   $(".most_recent .owl-carousel").owlCarousel({
     items: 2,
-    rtl: true,
+    rtl: dir,
     loop: true,
     margin: 15,
     nav: false,
@@ -96,6 +137,8 @@ $(document).ready(function () {
       }
     },
   });
+
+  // home counter
   $('.count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
@@ -107,20 +150,81 @@ $(document).ready(function () {
         }
     });
   });
+
+  // rateyo
   $(function () {
-    $(".Star-rate").rateYo({
-      starWidth: "15px",
-      ratedFill: "#FFC107",
-      readOnly: true,
-      normalFill: "#eee",
-      rtl:true
+    $(".your-comment #rateYo").rateYo({
+      starWidth: "20px",
+      ratedFill: "#FFD500",
+      normalFill: "#A7A7A7",
+      rtl: dir,
+      halfStar: false,
+      starSvg:'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"> <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/> </svg>'
     });
   });
+
+  // apply job upload cv
+  $('#file-input').change(function(){
+    const fileInput = $(this).find('[type="file"]')[0];
+    const label = $(this).find('[data-js-label]')[0];
+    console.log($(fileInput).val());
+    if (!$(fileInput).val()) return
+      var value = $(fileInput).val().replace(/^.*[\\\/]/, '')
+      $(label).html(value)
+  })
+
+  // textarea
+  var $txtArea = $('textarea');
+    $txtArea.on('keyup', countChar);
+    function countChar() {
+      var id =$(this).attr("id") ;
+      var chars = $(this).next("span");
+      var textMax = $(this).attr('maxlength');
+      var textLength = $(this).val().length;
+      var textRemaining = textLength;
+      chars.html(textMax + ' /' + textRemaining);
+    };
+
+  // toggle fav item
+  $(".fav").click(function () {
+    $(this).toggleClass("added");
+    $(this).toggleClass("bi-heart bi-heart-fill");
+    console.log(this)
+})
+$(".show-pass").click(function () {
+  $(this).find('i').toggleClass("bi bi-eye-slash bi-eye");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+      input.attr("type", "text");
+  } else {
+      input.attr("type", "password");
+  }
+  $(this).toggleClass('active');
+});
+  })
+
+  // follow 
+  $(".follow").click(function () {
+    $(this).toggleClass("unfollow");
+    $(this).find('span').toggleText('متابعة','الغاء المتابعة');
+    console.log(this)
+  })
+
+  // make auth-img circle 
+  var authWidth = $('.author-cover').width();
+  $('.author-cover').height(authWidth)
+  // -- responsive --
+  $(window).resize(function(){
+    var authWidth = $('.author-cover').width();
+  $('.author-cover').height(authWidth)
+  })
+
+
+  // niceSelect
   $('select').niceSelect();
 
   // wow.js init
   new WOW().init();
-});
 
 
 
