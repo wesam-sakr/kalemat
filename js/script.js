b33c78f42bd831fdeecb16210f545fe7ccfaadb3
wesam-sakr <wesamsakr801@gmail.com>
2023-06-26 11:25:14 +0300

    icons

--- js/script.js
+++ js/script.js
@@ -209,6 +209,7 @@ $(".show-pass").click(function () {
   // follow 
   $(".follow").click(function () {
     $(this).toggleClass("unfollow");
+    $(this).find('i').toggleClass("fa-thumbs-up fa-thumbs-down");
     $(this).find('span').toggleText('متابعة','الغاء المتابعة');
     console.log(this)
   })
